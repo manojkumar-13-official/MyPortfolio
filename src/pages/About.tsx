768f69acb7a89567e0000a7bf1eefe523106538a
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2025-11-15 20:03:17 +0530

    Add the content in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      
+      <h1>My name is manojkumar currently pursuing my bachelors degree in Computer Science and Engineering at RMD Enginerring College</h1>
     </div>
   )
 }
