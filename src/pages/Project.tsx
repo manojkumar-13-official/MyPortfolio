f0b2b664c47ca9172a6d5802ca09a0441e1b7391
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2026-01-15 21:55:51 +0530

    The Layout completion of About section

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -4,7 +4,7 @@ const Project = () => {
   return (
     <div>
       <h1 className='text-4xl font-bold text-center mt-20 text-gray-400 mb-5'>My Projects</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 relative'>
         {projectData.map(project => (
           <ProjectCard key={project.id} project={project} />
         ))}
