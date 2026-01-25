1888955a056460ef1016e044cb01203220019855
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2026-01-25 20:54:08 +0530

    some practices

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,6 +1,7 @@
 import projectData from '../Data/project.json'
 import ProjectCard from '@/components/ProjectCard'
 const Project = () => {
+
   return (
     <div className='min-h-screen'>
       <h1 className='text-4xl font-bold text-center mt-20 text-gray-400 mb-5'>My Projects</h1>
